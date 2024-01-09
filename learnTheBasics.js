a1f57420668e912b381d3a05ee206e1d010b79ac
Shashwat27sri <shashwatsu27@gmail.com>
2024-01-09 16:43:05 +0530

    symmetry void ,

--- learnTheBasics.js
+++ learnTheBasics.js
@@ -292,4 +292,107 @@ public class learnTheBasics {
             System.out.println();
         }
     }
+
+
+
+
+    // symmetric void
+
+    public static void symmetry(int n) {
+
+        first(n);
+
+        second(n);
+
+    }
+
+ 
+
+    static void first(int n){
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=n-i+1;j++){
+
+                System.out.print("* ");
+
+            }
+
+            for(int j=1;j<=2*i-2;j++){
+
+                System.out.print("  ");
+
+            }
+
+            for(int j=1;j<=n-i+1;j++){
+
+                System.out.print("* ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+    static void second(int n){
+
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+
+                System.out.print("* ");
+
+            }
+
+            for(int j=1;j<=2*(n-i);j++){
+
+                System.out.print("  ");
+
+            }
+
+            for(int j=1;j<=i;j++){
+
+                System.out.print("* ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+
+    // butterfly pattern
+
+    public static void symmetry(int n) {
+        // Write your code here
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            for (int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j = 1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+        for (int i =n-1;i>=1;i--){
+            for (int j =1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            for (int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j = 1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
 }
