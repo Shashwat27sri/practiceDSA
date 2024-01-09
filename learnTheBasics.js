12016a446b8f4fb029fe220f1b95f96620ae4232
Shashwat27sri <shashwatsu27@gmail.com>
2024-01-09 16:46:25 +0530

    hollow rhombus , getNumberPattern

--- learnTheBasics.js
+++ learnTheBasics.js
@@ -395,4 +395,49 @@ public class learnTheBasics {
             System.out.println();
         }
     }
+
+
+
+    // hollow rhombus
+    public static void getStarPattern(int n) {
+        // Write your code here
+        for (int i=1;i<=n;i++){
+            for (int j = 1;j<=n;j++){
+                if (i == 1 || i== n || j== 1 || j == n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+    // numberPattern
+    public static void getNumberPattern(int n) {
+
+        for(int i=0;i<2*n-1;i++){
+
+            for(int j=0;j<2*n-1;j++){
+
+                int top=i;
+
+                int left=j;
+
+                int right=(2*n-2)-j;
+
+                int down=(2*n-2)-i;
+
+                System.out.print(n-(Math.min(Math.min(top, down),Math.min(right, left))));
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
 }
