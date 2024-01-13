5a5022eaf0186941b1921e30c42b9773b8f7964e
Shashwat27sri <shashwatsu27@gmail.com>
2024-01-13 22:06:25 +0530

    add printIncNos

--- learnTheBasics.js
+++ learnTheBasics.js
@@ -658,4 +658,20 @@ public class learnTheBasics {
 
         
     }
+
+
+
+
+
+
+
+    public static int[] printNos(int x) {
+        // Write Your Code Here
+        int arr[]=new int[x];
+        for(int i = 0 ;i < x;i++){
+            arr[i] = i+1;
+
+        }
+        return arr;
+    }
 }
