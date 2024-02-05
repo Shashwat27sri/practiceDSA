19d25e800efb60340b1eef6bd070fb3aa8488a66
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-05 20:53:30 +0530

    add insertion sort

--- Sorting.java
+++ Sorting.java
@@ -46,17 +46,17 @@ public class Solution {
 
 
 
-  // // insertion sort
-  // public static void insertionSort(int[] arr, int size) {
-  // //Your code goes here
-  //   for (int i = 0 ; i<size-1; i++){
-  //       for (int j = size-1 ; j>0; j-- ){
-  //           if (arr[j-1] > arr[j]){
-  //               int temp = arr[j];
-  //               arr[j] = arr[j-1]; 
-  //               arr[j-1] = temp;
-  //           }
-  //       }
-  //   }
-  // }
+  // insertion sort
+  public static void insertionSort(int[] arr, int size) {
+  //Your code goes here
+    for (int i = 0 ; i<size-1; i++){
+        for (int j = size-1 ; j>0; j-- ){
+            if (arr[j-1] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j-1]; 
+                arr[j-1] = temp;
+            }
+        }
+    }
+  }
 }
