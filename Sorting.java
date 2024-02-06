db3741b5382fc5ef3f8903b83727c38e1784c06d
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-06 18:31:48 +0530

    add merge sort

--- Sorting.java
+++ Sorting.java
@@ -59,4 +59,97 @@ public class Solution {
         }
     }
   }
+
+
+
+
+
+    //merge Sort
+    public static void mergeSort(int[] arr, int l, int r){
+
+    // Write your code here
+
+    if(l<r){
+
+    int mid=(l+r)/2;
+
+    mergeSort(arr,l,mid);
+
+    mergeSort(arr,mid+1,r);
+
+
+
+    merge(arr,l,mid,r);
+
+    }
+
+
+
+    }
+
+    private static void merge(int[]arr,int l,int mid,int r){
+
+    int n1=mid-l+1;
+
+    int n2= r-mid;
+
+
+
+    int larr[]=new int[n1];
+
+    int rarr[]=new int[n2];
+
+    for(int x=0;x<n1;x++){
+
+    larr[x]=arr[l+x];
+
+    }
+
+    for(int x=0;x<n2;x++){
+
+    rarr[x]=arr[mid+1+x];
+
+
+
+    }
+
+    int i=0;
+
+    int j=0;
+
+    int k=l;
+
+    while(i<n1 && j<n2){
+
+    if(larr[i]<rarr[j]){
+
+    arr[k]=larr[i];
+
+    i++;
+
+    }
+
+    else{
+
+    arr[k]=rarr[j];
+
+    j++;
+
+    }
+
+    k++;
+
+    }
+
+    while(i<n1){
+
+    arr[k]=larr[i];
+
+    i++;
+
+    k++;
+
+    }
+
+    }   
 }
