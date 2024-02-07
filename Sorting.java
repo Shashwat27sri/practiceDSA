69f5daa2b83e3cc20be1bb1f5309ff4ff4347f54
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-07 23:09:31 +0530

    add recursive bubblesort

--- Sorting.java
+++ Sorting.java
@@ -152,4 +152,26 @@ public class Solution {
     }
 
     }   
+
+
+
+
+
+
+    // RECURSIVE BUBBLESORT
+
+    if (n == 1){
+            return;
+        }
+
+        for(int i = 0 ; i<=n-2 ; i++){
+            if (arr[i] > arr[i+1]){
+                int temp = arr[i]; 
+                arr[i] = arr[i+1]; 
+                arr[i+1]= temp; 
+            }
+
+        }
+
+        bubbleSort(arr , n-1);
 }
