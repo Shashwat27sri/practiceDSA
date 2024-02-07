7bbae29c276deb7b9be84100ea0cb86a6ab107e9
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-07 23:12:35 +0530

    add insertion sor t

--- Sorting.java
+++ Sorting.java
@@ -160,18 +160,52 @@ public class Solution {
 
     // RECURSIVE BUBBLESORT
 
+    public static void bubbleSort(int arr[ ] , int n) {
+
+
     if (n == 1){
             return;
         }
 
-        for(int i = 0 ; i<=n-2 ; i++){
-            if (arr[i] > arr[i+1]){
-                int temp = arr[i]; 
-                arr[i] = arr[i+1]; 
-                arr[i+1]= temp; 
-            }
-
+    for(int i = 0 ; i<=n-2 ; i++){
+        if (arr[i] > arr[i+1]){
+            int temp = arr[i]; 
+            arr[i] = arr[i+1]; 
+            arr[i+1]= temp; 
         }
 
-        bubbleSort(arr , n-1);
+    }
+
+    bubbleSort(arr , n-1);
+
+    }
+
+
+
+
+
+    // RECURSIVE INSERTION SORT
+
+    public static void insertionSort(int[] arr , int size) {
+        helper(arr,0,size);
+
+    }
+
+    public static void helper(int[] arr , int i , int n  ){
+
+        // recursive approach
+        if(i == n ){
+            return; 
+        }
+        int j = i ;
+        while(j>0 && arr[j-1] > arr[j]){
+            int temp = arr[j]; 
+            arr[j] =arr[j-1];
+            arr[j-1] = temp; 
+            j--;
+        }
+
+        helper(arr , i+1 , n );
+    }
+
 }
