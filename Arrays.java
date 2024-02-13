7e4802e5bda8ee9151b2585ca4c8a4002d5113ad
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-14 00:37:58 +0530

    add linear search

--- Arrays.java
+++ Arrays.java
@@ -99,4 +99,21 @@ public class Solution {
         }
         return i+1;
     }
+
+
+
+
+
+
+
+    // linear search
+    public static int linearSearch(int n, int num, int []arr){
+        // Write your code here.
+        for (int i = 0 ;i< n ;i++){
+            if (arr[i] == num){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
