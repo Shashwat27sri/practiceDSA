315f38e2222d0ce474586195a042f2063d983fec
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-14 19:00:19 +0530

    add move zeroes to end

--- Arrays.java
+++ Arrays.java
@@ -102,6 +102,29 @@ public class Solution {
 
 
 
+    // move zeroes to end 
+    public static int[] moveZeros(int n, int []a) {
+        // Write your code here.
+        ArrayList<Integer> temp  = new ArrayList<>(); 
+        
+        for (int i = 0 ; i< n ; i++ ){
+            if (a[i] != 0 ){
+                temp.add(a[i]);
+            }
+        }
+        
+        int nz = temp.size();
+        
+        for (int i=0;i < nz; i++ ){
+            a[i] = temp.get(i); 
+        }
+        for (int i = nz; i < n; i++){
+            a[i] = 0;
+        }
+        return a;
+    }
+
+
 
 
 
