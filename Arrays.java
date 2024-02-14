bb8c355746116f5b9e21d95af56e7217e53a9d69
Shashwat27sri <shashwatsu27@gmail.com>
2024-02-14 21:41:11 +0530

    add left rotate by 1 step

--- Arrays.java
+++ Arrays.java
@@ -127,6 +127,21 @@ public class Solution {
 
 
 
+    //left rotate by 1 step
+    
+    static int[] rotateArray(int[] arr, int n) {
+        // Write your code here.
+        int temp =arr[0];
+        for (int  i=0 ;i< n-1 ;i++){
+            arr[i] = arr[i+1];
+
+        }
+        arr[n-1]= temp;
+        return arr;
+    }
+
+
+
 
 
     // linear search
